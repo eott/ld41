291d26e528a58a667abb86dfb96e9ed0036fffd6
Etienne Ott <eti.ott@gmail.com>
2018-04-21 22:59:05 +0200

    Extent input functionality.

--- input.js
+++ input.js
@@ -2,13 +2,27 @@ Input = function() {
     // w a s d space left right up down
     this.keyStatus = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+    this.keyCodes = {
+        "w": 87,
+        "a": 65,
+        "s": 83,
+        "d": 68,
+        "space": 32,
+        "left": 37,
+        "right": 39,
+        "up": 38,
+        "down": 40
+    }
+
+    this.keyIndizes = [87, 65, 83, 68, 32, 37, 39, 38, 40];
+
     this.registerListeners()
 }
 
 Input.prototype.keyEvent = function(type, key) {
     // w = 87, a = 65, s = 83, d = 68, space = 32
     // left = 37, right= 39, up = 38, down = 40
-    var index = [87, 65, 83, 68, 32, 37, 39, 38, 40].indexOf(key)
+    var index = this.keyIndizes.indexOf(key)
     
     if (index == -1) {
         return
@@ -23,7 +37,22 @@ Input.prototype.keyEvent = function(type, key) {
     }
 }
 
+Input.prototype.keyWasPressed = function(keyname) {
+    var index = this.keyIndizes.indexOf(this.keyCodes[keyname])
+    
+    if (index == -1) {
+        return false
+    }
+
+    if (this.keyStatus[index] == 1) {
+        this.keyStatus[index] = 2
+        return true
+    } else {
+        return false
+    }
+}
+
 Input.prototype.registerListeners = function() {
-    window.addEventListener("keydown", function(e) {this.keyEvent(1, e.keyCode);}, false)
-    window.addEventListener("keyup", function(e) {this.keyEvent(0, e.keyCode);}, false)
+    window.addEventListener("keydown", function(e) {this.keyEvent(1, e.keyCode)}.bind(this), false)
+    window.addEventListener("keyup", function(e) {this.keyEvent(0, e.keyCode)}.bind(this), false)
 }
